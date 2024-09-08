3e4cd896520d82039567f0b88e29450fb88b7ce8
knox <voidisback4@gmail.com>
2024-09-08 20:41:56 +0800

    added search by tags feature using javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+import SearchComponent from './SearchComponent'; // Adjust the import path as needed
+
+function App() {
+  return (
+    <div>
+      <h1>DinoCore</h1>
+      <SearchComponent />
+    </div>
+  );
+}
+
+export default App;
